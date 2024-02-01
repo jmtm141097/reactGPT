171f26393dac330fa4c57e519eb77e5cf1819fc0
Juan Manuel Tamayo Monje <jmtm141097@gmail.com>
2024-02-01 07:06:46 -0500

    add template for send messsages

--- src/presentation/components/index.tsx
+++ src/presentation/components/index.tsx
@@ -1,1 +1,5 @@
 export * from "./sidebar/SidebardMenuItem"
+export * from "./chat-bubbles/GptMessage"
+export * from "./chat-bubbles/MyMessage"
+export * from "./loaders/TypingLoader"
+export * from "./chat-input-boxes/TextMessageBox"
